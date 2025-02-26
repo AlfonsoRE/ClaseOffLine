2d92fefd41ed11877f13f014898fab94993a9696
Alfonso Rosas Escobedo <isc_alfonsore@hotmail.com>
2025-02-26 17:32:01 -0600

    se sube el servicio de consultar un usuario, modificar y eliminar

--- api/consultarUsuario.php
+++ api/consultarUsuario.php
@@ -1,11 +1,14 @@
 <?php
 error_reporting(E_ALL);
 require_once 'conexion.php';
-$stmt = $db->prepare("SELECT id,nombre,email, password, rol, status, fecha_inicio FROM usuarios");
+$obj = json_decode(file_get_contents("php://input")); 
+$stmt = $db->prepare("SELECT id,nombre,email, password, rol, status, fecha_inicio FROM usuarios 
+WHERE id = ?");
+$stmt->bind_param('i',$obj->id);
 $stmt->bind_result($id,$nombre,$email,$password,$rol,$status,$fecha_inicio);
 $stmt->execute();
 $arr = array();
-while($stmt->fetch()){
+if($stmt->fetch()){
 $arr[] = array('id' =>$id, 
 'nombre' =>$nombre,
 'email' =>$email,
--- api/guardarUsuario.php
+++ api/guardarUsuario.php
@@ -3,9 +3,9 @@ error_reporting(E_ALL);
 require_once 'conexion.php';
 $obj = json_decode(file_get_contents("php://input")); 
 $stmt=$db->prepare("INSERT INTO usuarios(nombre,email,password,rol,fecha_inicio) 
- VALUES(?,?,?,?,NOW())");
+ VALUES(?,?,?,'usuario',NOW())");
 $pass = md5($obj->password);
-$stmt->bind_param('ssss',$obj->nombre,$obj->email,$pass,$obj->rol);
+$stmt->bind_param('sss',$obj->nombre,$obj->email,$pass);
 $stmt->execute();
 $stmt->close();
 echo "Registro exitoso";
