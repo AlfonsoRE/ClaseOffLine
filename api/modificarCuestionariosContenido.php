a6dab6d7832ca1cb289a19026614de592b2ebb66
Adan Noe <adannoe03@outlook.com>
2025-03-04 18:24:21 -0600

    Este es una pequeña Modificacion

--- api/modificarCuestionariosContenido.php
+++ api/modificarCuestionariosContenido.php
@@ -4,7 +4,7 @@ require_once 'conexion.php';
 
 $obj = json_decode(file_get_contents("php://input"));
 $stmt = $db->prepare("UPDATE cuestionarios_contenido SET pregunta=?, opcion1=?, opcion2=?, opcion3=?, opcion4=?, respuesta=? WHERE id=?");
-$stmt->bind_param('isssssi', $obj->pregunta, $obj->opcion1, $obj->opcion2, $obj->opcion3, $obj->opcion4, $obj->respuesta, $obj->id);
+$stmt->bind_param('ssssssi', $obj->pregunta, $obj->opcion1, $obj->opcion2, $obj->opcion3, $obj->opcion4, $obj->respuesta, $obj->id);
 $stmt->execute();
 $stmt->close();
 echo "Registro modificado";
