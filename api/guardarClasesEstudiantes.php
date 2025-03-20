62c298126a9fffd49e5314c70442aebdf89c955b
Alfonso Rosas Escobedo <isc_alfonsore@hotmail.com>
2025-03-20 17:52:52 -0600

    se modico web service de clases estudiantes

--- api/guardarClasesEstudiantes.php
+++ api/guardarClasesEstudiantes.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL);
 require_once 'conexion.php';
 $obj = json_decode(file_get_contents("php://input")); 
-$stmt=$db->prepare("INSERT INTO clase_estudiantes(id_clase,id_estudiante,status) 
- VALUES(?,?,?)");
-$stmt->bind_param('iis',$obj->id_clase,$obj->id_estudiante,$obj->status);
+$stmt=$db->prepare("INSERT INTO clase_estudiantes(id_clase,id_estudiante) 
+ VALUES(select id from clases where codigo= ?,?)");
+$stmt->bind_param('si',$obj->codigo,$obj->id_estudiante);
 $stmt->execute();
 $stmt->close();
 echo "Registro exitoso";
