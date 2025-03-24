0f0f9a56e8a88da3a6dcc6849304f624e8930def
AdanCorro <adannoe03@outlook.com>
2025-03-24 16:51:54 -0600

    Este es el Api Corregido

--- api/guardarClasesEstudiantes.php
+++ api/guardarClasesEstudiantes.php
@@ -2,9 +2,10 @@
 error_reporting(E_ALL);
 require_once 'conexion.php';
 $obj = json_decode(file_get_contents("php://input")); 
-$stmt=$db->prepare("INSERT INTO clase_estudiantes(id_clase,id_estudiante) 
- VALUES(select id from clases where codigo= ?,?)");
-$stmt->bind_param('si',$obj->codigo,$obj->id_estudiante);
+$stmt = $db->prepare("
+    INSERT INTO clase_estudiantes (id_clase, id_estudiante) 
+    SELECT id, ? FROM clases WHERE codigo = ?");
+$stmt->bind_param('is', $obj->id_estudiante, $obj->codigo);
 $stmt->execute();
 $stmt->close();
 echo "Registro exitoso";
